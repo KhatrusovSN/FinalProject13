9c52ef4bfc8a63bab30a2d5f33b6b76004c21d69
Sergey <Sergey@DESKTOP-1G83SGB>
2022-12-10 23:24:17 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static string path = "C:\\Users\\Sergey\\Downloads\\Text1.txt";
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch(); // Создание обьекта 
+            var stringList = new List<string>(); // Создание списка
+            string text = File.ReadAllText(path); // Запись тестового текста в переменную
+
+            stopWatch.Start(); //Старт 
+            foreach (string word in text.Split(" ")) // Заполнение списка словами из текста
+            {
+                stringList.Add(word);
+            }
+            stopWatch.Stop(); // Остановка таймера
+            Console.WriteLine($"Затрачено на добавление текста в List = {stopWatch.Elapsed.TotalMilliseconds} ms");
+
+            stopWatch.Reset(); // Обуление таймера
+
+            var stringLinkedList = new LinkedList<string>(); // Создание связаного списка
+
+            stopWatch.Start();
+            foreach (string word in text.Split(" "))
+            {
+                stringLinkedList.AddLast(word);
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"Затрачено на добавление текста в LinkedList = {stopWatch.Elapsed.TotalMilliseconds} ms");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static string path = "C:\\Users\\Sergey\\Downloads\\Text1.txt";
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(path);
+            string? noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            char[] charsDel = new char[] { ' ', '\n' };
+
+            string[]? words = noPunctuationText.Split(charsDel, StringSplitOptions.RemoveEmptyEntries);
+            var repeatedWords = new Dictionary<string, int>();
+
+            foreach ( var word in words )
+            {
+                bool cont = repeatedWords.ContainsKey(word);
+                int count;
+
+                if (cont)
+                {
+                    repeatedWords.TryGetValue(word, out count);
+                    repeatedWords.Remove(word);
+                    repeatedWords.Add(word, count + 1);
+                }
+                else
+                {
+                    repeatedWords.Add(word, 1);
+                }
+            }
+
+            int i = 0;
+            foreach (var word in repeatedWords.OrderByDescending(key => key.Value))
+            {
+                if (i < 10)
+                {
+                    Console.WriteLine($"Word: {word.Key} \t Repeats: {word.Value}");
+                    i++;
+                }
+            }
+        }
+    }
+}
